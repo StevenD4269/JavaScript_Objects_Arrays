f535c5e59915cbf8bceaeb675bf2823e86937875
Steven Dickerson <poddocdan@gmail.com>
2024-04-04 12:42:13 -0700

    Plugged in the variables and parameters

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+function arraySum(numbers) {
+    let sum = 0;
+    numbers.forEach((number, index) => { XXXXX};
+    return sum;
+        
+};
+
+
+
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
