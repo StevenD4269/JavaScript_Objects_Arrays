d9614fea5b4a71de6eb30772b100f2e2854a81cf
Steven Dickerson <poddocdan@gmail.com>
2024-04-08 06:57:01 -0700

    start ex4

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-// console.log("Hello World!:\n==========\n");
-// // Exercise 1 Section
-// console.log("EXERCISE 1:\n==========\n");
+console.log("Hello World!:\n==========\n");
+// Exercise 1 Section
+console.log("EXERCISE 1:\n==========\n");
+// the first example
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+function arraySum(nums) {
+  let sum = 0;
+  // the "easy" way
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+}
+
+// hard way
 // const numbers = [2, 22, 12, 17, 18, 39, 129];
 // function arraySum(numbers) {
 //   let sum = 0;
@@ -11,18 +22,20 @@
 // }
 // console.log(arraySum(numbers));
 
-// // Exercise 2
-// const book = {};
-// book.title = "Mistborn";
-// book.numPages = 647;
-// book.readCount = 3;
+// Exercise 2
+console.log("EXERCISE 2:\n==========\n");
+const book = {};
+book.title = "Mistborn";
+book.numPages = 647;
+book.readCount = 3;
 
-// book.info = function () {
-//   return `${this.title} by Brandon Sanderson, ${this.numPages}, read ${this.readCount} times.`;
-// };
-// console.log(book.info());
+book.info = function () {
+  return `${this.title} by Brandon Sanderson, ${this.numPages}, read ${this.readCount} times.`;
+};
+console.log(book.info());
 
 // Exercise 3
+console.log("EXERCISE 3:\n==========\n");
 let sentence = "The quick brown fox jumps over the lazy dog";
 
 // Step 2: use the split command and a space between " " will split them into an array
@@ -40,3 +53,25 @@ for (let i = 0; i < arrayOfWords.length; i++) {
 // convert back to a string
 let reversedSentence = arrayOfWords.join(" ");
 console.log(reversedSentence);
+
+// // Exercise 4
+console.log("EXERCISE 4:\n==========\n");
+let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
+
+// Step 2: use the split command to separate each string into its own row or index. NOTE*** name, age is ONE STRING, same as Frodo,50 etc
+let turnLinesToRows = csvData.split("\n");
+// will now look like ['name,age', 'Frodo,50', 'Sam,38', 'Merry,36', 'Pippin,26'], so 5 strings/indexes
+
+// Step 3: create new variable that takes your new rows and separates them into its own header, 0 is the first index aka "name,age"  that it accesses
+// turnlinestorows[0] accesses the first index
+// the split(",") command is really taking ["name,age"] and turning it into ["name", "age"], so substrings of the strings, so one string becomes two
+let firstRowSplitIntoArray = turnLinesToRows[0].split(",");
+
+// // Step 4: create a for loop to iterate over each array
+for (let i = 0; i < turnLinesToRows.length; i++) {
+  //
+  let splitTheData = csvData.slice(firstRowSplitIntoArray);
+  console.log(splitTheData);
+}
+
+// console.log(splitTheData);
